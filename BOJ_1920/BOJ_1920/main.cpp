6fcc10e3ab08c54bd0eecec7b157f2bddc30b417
Jwon <check4545@daum.net>
2022-05-05 01:06:45 +0900

    Initial commit

---
+++ BOJ_1920/BOJ_1920/main.cpp
@@ -0,0 +1,56 @@
+//
+//  main.cpp
+//  BOJ_1920
+//
+//  Created by 최주원 on 2022/05/05.
+//
+
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int A[100001];
+
+int binarySearch(int len, int x){
+    
+    int start = 1;
+    int end = len;
+    int mid;
+    
+    while(end - start >= 0){
+        
+        mid = (start + end) / 2;
+        
+        if(A[mid] == x){
+            return 1;
+        }
+        else if(A[mid] > x){
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    
+    return 0;
+}
+
+int main() {
+    
+    int n, count, x;
+    
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &A[i]);
+    }
+    sort(A + 1, A + n + 1);
+    
+    scanf("%d", &count);
+    for (int j = 1; j <= count; j++) {
+        scanf("%d", &x);
+        printf("%d\n", binarySearch(n, x));
+    }
+    
+    return 0;
+}
