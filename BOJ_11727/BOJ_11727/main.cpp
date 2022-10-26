c14e71cc9e3c080418df501b2cb46b22de12ced1
Jwon <check4545@kakao.com>
2022-10-27 00:02:02 +0900

    Solved BOJ 11727

---
+++ BOJ_11727/BOJ_11727/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    
+    int arr[1001];
+    int width;
+    
+    arr[1] = 1;
+    arr[2] = 3;
+    
+    for(int i = 3; i <= 1000; i++){
+        arr[i] = arr[i - 1] + arr[i - 2] * 2;
+        arr[i] %= 10007;
+    }
+    
+    scanf("%d", &width);
+    printf("%d\n", arr[width] % 10007);
+    
+}
