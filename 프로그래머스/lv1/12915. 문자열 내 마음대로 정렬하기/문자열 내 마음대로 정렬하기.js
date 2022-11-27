c773dc3b1b0d1fe3e0a7a5e4e2706debe50a65c2
Jwon <check4545@daum.net>
2022-11-27 16:11:32 +0900

    Update 문자열 내 마음대로 정렬하기.js

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,27 +1,12 @@
 function solution(strings, n) {
-    var answer = [];
-    
     
     strings.sort(
-        (a, b) => 
-        {
-            if(a[n].charCodeAt() > b[n].charCodeAt()){
-                return 1
-            } 
-            
-            else if(a[n].charCodeAt() == b[n].charCodeAt()){
-                return (a > b) ? (1) : (-1)
-            }
-            
-            else{
-                return -1
-            }
-        }
-
-        
+        (a, b) => {
+            if(a[n].charCodeAt() > b[n].charCodeAt()) return 1;
+            else if(a[n].charCodeAt() == b[n].charCodeAt()) return (a > b) ? (1) : (-1);
+            else return -1;
+        }   
     )
-    
-    console.log(strings)
     
     return strings;
 }
