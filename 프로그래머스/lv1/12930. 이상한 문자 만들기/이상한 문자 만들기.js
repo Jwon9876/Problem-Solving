770d50e7d25e7dbdf8123c877360f56fc370df40
Jwon <check4545@kakao.com>
2022-11-29 13:27:15 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 4.79 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,27 @@
+function solution(s) {
+    var answer = '';
+    
+    var wordIndex = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        
+
+        if(s[i] == ' '){
+            wordIndex = 0;
+            answer += " ";
+        } else{
+            wordIndex++;            
+            (wordIndex % 2 == 1) ? (answer += s[i].toUpperCase()) : (answer += s[i].toLowerCase());
+        }
+        
+        console.log(wordIndex)        
+    }
+    
+    
+    
+    
+    
+    
+    
+    return answer;
+}
