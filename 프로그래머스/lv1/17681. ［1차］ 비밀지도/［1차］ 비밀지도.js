d5f418f02d0cb5b07bbab4b9ea64d86c3f45b10c
Jwon <check4545@kakao.com>
2022-12-02 14:12:34 +0900

    Fix

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -3,12 +3,9 @@ function solution(n, arr1, arr2) {
     
     for(let i = 0; i < arr1.length; i++){
         answer.push((arr1[i] | arr2[i]).toString(2).padStart(n, 0))
-    }
-    
-    console.log(answer)
+    }    
     
     for(let j = 0; j < answer.length; j++){
-        console.log(answer[j].replaceAll("1", "#"))
         answer[j] = answer[j].replaceAll("1", "#")
         answer[j] = answer[j].replaceAll("0", " ")
     }
