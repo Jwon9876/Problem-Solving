fdbed0e6f3f87a4c9ac5285df24fc55d8e5a5179
Jwon <check4545@kakao.com>
2022-12-04 22:43:17 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,25 @@
+
+// 3중 포문 돌리면 될 듯
+
+
+function solution(number) {
+    var answer = 0;
+    
+    
+    
+    for(let i = 0; i < number.length - 2; i++){
+        for(let j = i + 1; j < number.length - 1; j++){
+            for(let k = j + 1; k < number.length; k++){
+                if(number[i] + number[j] + number[k] == 0){
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    return answer;
+}
