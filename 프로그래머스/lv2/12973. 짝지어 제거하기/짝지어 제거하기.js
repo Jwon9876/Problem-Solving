1714500353d7ea580f6cf864615a3a5434422a08
Jwon <check4545@kakao.com>
2022-12-07 14:08:39 +0900

    Update 짝지어 제거하기.js

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,18 +1,20 @@
 function solution(s)
 {
+ 
+    if(s.length % 2 == 1) return 0;
     
-    var arr = []
-    s = s.split("")
+    var arr = [];
+    s = s.split("");
     
     for(let i = 0; i < s.length; i++){
         if(!arr.length) arr.push(s[i])
         else if(arr[arr.length - 1] == s[i]){
-            arr.pop()
+            arr.pop();
         } else{
-            arr.push(s[i])
+            arr.push(s[i]);
         }
     }
-    console.log(arr)
+    
     
 
     return arr.length ? 0 : 1;
