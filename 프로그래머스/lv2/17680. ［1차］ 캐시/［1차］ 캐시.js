6806d2fa4c4656b414436a8f42a0e21347438961
Jwon <check4545@kakao.com>
2022-12-12 21:17:35 +0900

    Update ［1차］ 캐시.js

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -14,23 +14,14 @@ function solution(cacheSize, cities) {
     
     // 있으면 => +1, 배열의 맨 앞으로 이동
     
-    
-    // unshift -> 안쓰고 구현해야 함.
-    // unshift 메서드는 배열 형태의 객체 시작점에 주어진 값을 삽입합니다.
-
-    // unshift는 제네릭하도록 설계되었으며, 배열 형태를 가진 객체가 호출하거나 객체에 적용할 수 있습니다. 
-    // length 속성을 가지지 않고, 대신 마지막 요소를 0부터 시작하는 순차적 인덱스로만 나타내는 객체에서는 의도한 것과 다르게 행동할 수 있습니다.
-    
     for(let i = 0; i < cities.length; i++){
         
-        let convertedCity = cities[i].toUpperCase()
+        let convertedCity = cities[i].toUpperCase();
         
         if(!cacheSize){
             answer += 5;
         } else if(!cache.includes(convertedCity)){
             if(cache.length == cacheSize && cache.length > 0){
-                let index = cache.indexOf(convertedCity);
-                // TODO
                 cache.splice(0, 1);
             }
             cache.push(convertedCity);
@@ -38,14 +29,10 @@ function solution(cacheSize, cities) {
             
         } else if(cache.includes(convertedCity)){
             let index = cache.indexOf(convertedCity);
-                // TODO
             cache.splice(index, 1);
             cache.push(convertedCity);
-            
             answer += 1;
-
         }
-        
         
     }
     
