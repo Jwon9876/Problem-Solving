6d555c363f03e23dd0af4c5f314a88eec5488eb2
Jwon <check4545@kakao.com>
2022-12-19 14:43:07 +0900

    Update H－Index.js

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,14 +1,15 @@
 function solution(citations) {
     var answer = 0;
     
-    citations.sort((a, b) => (b - a))
+    citations.sort((a, b) => (b - a));
     
     for(let i = 0; i < citations.length; i++){
-        if(citations[i] >= i + 1)
-            answer++;
-        
+        if(citations[i] >= i + 1){
+            answer++;            
+        } else{
+            break;
+        }
     }
-    
     
     return answer;
 }
