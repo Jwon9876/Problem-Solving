1f465ccf1eb15780ed9b0282555ae43159a04b53
Jwon <check4545@kakao.com>
2022-12-23 21:08:29 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,17 @@
+function solution(my_string, n) {
+    var answer = '';
+    
+    for(str of my_string){
+        for(let i = 0; i < n; i++){
+           answer += str 
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    return answer;
+}
