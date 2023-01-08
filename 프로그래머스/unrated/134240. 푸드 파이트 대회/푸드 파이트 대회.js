f955e10040467f5d4abd3f9f7b45911efc962fcb
Jwon <check4545@kakao.com>
2023-01-08 12:16:23 +0900

    Update 푸드 파이트 대회.js

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,25 +1,43 @@
-function getFoodOrderStr(index, value){
-    
-    let foodOrderStr = "";
-    
-    for(let i = 0; i < Math.floor(value / 2); i++){
-        foodOrderStr += index;
-    }
-    
-    return foodOrderStr;
-}
 
-
-
+// refactored
 function solution(food) {
     
     var answer = '';
     
     for(let i = 1; i < food.length; i++){
-        answer += getFoodOrderStr(i, food[i]);
+        answer += String(i).repeat(Math.floor(food[i] / 2));
     }
     
     answer = answer + "0" + answer.split("").reverse().join("")
     
     return answer;
 }
+
+
+
+// previous
+// function getFoodOrderStr(index, value){
+    
+//     let foodOrderStr = "";
+    
+//     for(let i = 0; i < Math.floor(value / 2); i++){
+//         foodOrderStr += index;
+//     }
+    
+//     return foodOrderStr;
+// }
+
+
+
+// function solution(food) {
+    
+//     var answer = '';
+    
+//     for(let i = 1; i < food.length; i++){
+//         answer += getFoodOrderStr(i, food[i]);
+//     }
+    
+//     answer = answer + "0" + answer.split("").reverse().join("")
+    
+//     return answer;
+// }
