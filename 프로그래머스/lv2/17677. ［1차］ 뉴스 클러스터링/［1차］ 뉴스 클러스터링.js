6eb6cb0290dcd205e9a38bc2d1d63f4265be8bf3
Jwon <check4545@kakao.com>
2023-01-27 23:45:37 +0900

    Remove Comment

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -26,16 +26,8 @@ function solution(str1, str2) {
     
     let unionArr = [];
     let intersectionArr = [];
-    
-//     1 1 2 2 3
-//     1 2 2 4 5
-//     1 2 2
-//     1 1 2 2 3 4 5
-    
-//     1 2 3 / 1 2 4 5
-//     1 1 2 2 3 4 5
-    
-    
+    let unionAllArr = [];
+        
     for(let i = 0; i < splitStr1Arr.length; i++){
         
         let splitStr = splitStr1Arr[i];
@@ -53,24 +45,12 @@ function solution(str1, str2) {
     
     splitStr2Arr.forEach((v) => unionArr.push(v))
     
-    let unionAllArr = [...unionArr, ...intersectionArr];
-    
-    console.log(splitStr1Arr, splitStr2Arr)
-    console.log("___________")
-    console.log(intersectionArr, unionArr, unionAllArr)
-    
+    // A, B의 합집합은
+    // 중복 제거된 A + 중복 제거된 B 의 원소의 중복 허용된 합집합
+    unionAllArr = [...unionArr, ...intersectionArr];
     
     const JaccardSimilarity = parseInt((intersectionArr.length / unionAllArr.length) * 65536)
     
-    console.log(JaccardSimilarity)
-    
-    // A, B의 합집합은
-    // 중복 제거된 A + 중복 제거된 B 의 원소의 중복 허용된 합집합
-    
-
-
-
-
     
     
     return JaccardSimilarity;
