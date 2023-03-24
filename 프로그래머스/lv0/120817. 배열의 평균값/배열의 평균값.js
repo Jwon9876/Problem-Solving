c5074b041a08c391334f8f265e0d2c7dcb9df85c
Jwon <check4545@kakao.com>
2023-03-24 23:15:26 +0900

    Update 배열의 평균값.js

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,6 +1,3 @@
 function solution(numbers) {
-    var answer = 0;
-    const sum  = numbers.reduce((a, v, i) => a + v, 0);
-    console.log(Math.floor(sum / numbers.length))
-    return sum / numbers.length;
+    return numbers.reduce((a, v, i) => a + v, 0) / numbers.length;
 }
