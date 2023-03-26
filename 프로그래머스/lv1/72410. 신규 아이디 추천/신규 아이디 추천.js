3fc8874ef648116ed30d1c24c977a18c1509853e
Jwon <check4545@kakao.com>
2023-03-27 00:06:54 +0900

    Update 신규 아이디 추천.js

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,41 +1,26 @@
-const deleteComma = (str) => {
-    if(str[str.length - 1] == ".") return deleteComma(str.split("").splice(0, str.length - 1).join(""));
-    return str;
-}
-
-function solution(new_id) {
-    var answer = '';
+const solution = (newId) => {
     
-    // 1단계
-    answer = new_id.toLowerCase();
-
-    // 2단계
-    answer = answer.replaceAll(/[^a-z\d-_.]/g, "")
+    const deleteComma = (answer) => {
+        if(answer[answer.length - 1] == ".") return deleteComma(answer.split("").splice(0, answer.length - 1).join(""));
+        return answer;
+    }
     
-    // 3단계
-    answer = answer.replaceAll(/\.{2,}/g, ".")
- 
-    // 4단계
-    answer = answer.replaceAll(/^\.*/g, "")
+    let answer = '';
+    
+    // 1 - 4단계
+    answer = newId.toLowerCase().replaceAll(/[^a-z\d-_.]/g, "").replaceAll(/\.{2,}/g, ".").replaceAll(/^\.*/g, "");
     
     // 5단계
     if(answer == "") answer += "a";
     
     // 6단계
-    if(answer.length > 15) {
-        answer = answer.split("").slice(0, 15).join("");
-        console.log("adsdasdasdasdas")
-        console.log(answer)
-    }
-    
-    answer = deleteComma(answer)
+    if(answer.length > 15) answer = answer.split("").slice(0, 15).join("");
+    answer = deleteComma(answer);
     
     // 7단계
     while(answer.length < 3){
-        answer += answer[answer.length - 1]
+        answer += answer[answer.length - 1];
     }
-    
-    
     
     return answer;
 }
