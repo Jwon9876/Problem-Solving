816b978f679afbd6bf2ac0635867966c3ae1bb96
Jwon <check4545@kakao.com>
2023-11-24 13:10:31 +0900

    [Bronze I] Title: 당신은 운명을 믿나요?, Time: 144 ms, Memory: 21388 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const [str] = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+
+const KOREA = "KOREA"
+const YONSEI = "YONSEI"
+const arr = str.split("");
+
+let koreaStr = "";
+let yonseiStr = "";
+
+let k = 0;
+let y = 0;
+let id = 0;
+
+while (id < arr.length){
+
+    if(arr[id] == KOREA[k]){
+        koreaStr += arr[id];
+        k++
+    } else if(arr[id] == YONSEI[y]){
+        yonseiStr += arr[id];
+        y++
+    }
+
+    if(koreaStr == KOREA){
+        console.log(KOREA);
+        break;
+    } else if(yonseiStr == YONSEI){
+        console.log(YONSEI);
+        break;
+    }
+
+    id++;
+}
