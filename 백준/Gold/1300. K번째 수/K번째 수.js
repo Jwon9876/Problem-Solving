09a5709da1d0faf42e31bb257243df91ce008417
Jwon <check4545@kakao.com>
2024-03-05 10:57:55 +0900

    [Gold I] Title: K번째 수, Time: 892 ms, Memory: 63536 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.js
@@ -0,0 +1,25 @@
+const a = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
+
+const [n, k] = a.map(Number);
+
+let start = 1;
+let end = n * n;
+let result = 0;
+
+while(start <= end){
+	let mid = Math.floor((start + end) / 2);
+	let count = 0;
+	for (let i = 1; i <= n; i++) {
+		count += Math.min(n, parseInt(mid / i));
+	}
+	
+	if(count >= k){
+		result = mid
+		end = mid - 1;
+	} else{
+		start = mid + 1;
+	}
+}
+
+
+console.log(result)
