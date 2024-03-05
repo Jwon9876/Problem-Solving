02305efb48856d42ea11f8791f7511737abb3def
Jwon <check4545@kakao.com>
2024-03-05 23:29:42 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 660 ms, Memory: 135204 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
@@ -4,31 +4,27 @@ const n = +a;
 const arr = b.split(" ").map(Number);
 
 const lowerBound = (arr, target) => {
-	
 	let start = 0;
 	let end = arr.length;
 	
 	while(start < end){
 		const mid = Math.floor((start + end) / 2);
-		
 		if(arr[mid] >= target) end = mid;
 		else start = mid + 1;
 	}
 	return end;
-}
+};
 
 const result = [];
 
-result.push(arr[0]);
-
-arr.slice(1).forEach((v, i) => {
+arr.forEach((v, i) => {
 	if(result.at(result.length - 1) < v){
 		result.push(v)
 	} else {
 		const id = lowerBound(result, v)
 		result[id] = v;
 	}
-})
+});
 
 
-console.log(result.length)
+console.log(result.length);
